a3e4fd11c0054b5ee8f6e7d17bc4361d45b8fc18
TommyYehCool <tommy.yeh1112@gmail.com>
2017-01-09 15:56:26 +0800

    新增使用 MongoTemplate 查詢的測試

--- src/test/java/com/weatherrisk/test/Test_MongoDB_Consume.java
+++ src/test/java/com/weatherrisk/test/Test_MongoDB_Consume.java
@@ -3,6 +3,7 @@ package com.weatherrisk.test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Date;
+import java.util.List;
 
 import org.junit.FixMethodOrder;
 import org.junit.Test;
@@ -11,6 +12,9 @@ import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.weatherrisk.api.Application;
@@ -28,6 +32,9 @@ public class Test_MongoDB_Consume {
 	@Autowired
 	private ConsumeRepository consumeRepository;
 	
+	@Autowired
+	private MongoTemplate mongoTemplate;
+	
 	@Test
 	public void test_1_addConsume() throws Exception {
 		String lotteryNo = "12345678";
@@ -40,6 +47,9 @@ public class Test_MongoDB_Consume {
 		Boolean got = false;
 		Boolean alreadySent = false;
 		consumeRepository.save(new Consume(lotteryNo, user, consumeDate, type, prodName, amount, prize, got, alreadySent));
+		
+		lotteryNo = "87654321";
+		consumeRepository.save(new Consume(lotteryNo, user, consumeDate, type, prodName, amount, prize, got, alreadySent));
 	}
 	
 	@Test
@@ -49,5 +59,25 @@ public class Test_MongoDB_Consume {
 		System.out.println(consume);
 		assertThat(consume).isNotNull();
 		assertThat(consume.getLotteryNo()).isEqualTo(lotteryNo);
+	}
+	
+	/**
+	 * 參考: <a href="https://tests4geeks.com/spring-data-boot-mongodb-example/">Spring data boot mongodb example</a>
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void test_3_findConsumeByExample() throws Exception {
+		String lotteryNo = "12345678";
+
+		List<Consume> consumes 
+			= mongoTemplate.find(
+					Query.query(
+							new Criteria()
+								.orOperator(Criteria.where("lotteryNo").regex(lotteryNo, "i"))), 
+					Consume.class);
+
+		assertThat(consumes.size()).isEqualTo(1);
+		assertThat(consumes.get(0).getLotteryNo()).isEqualTo(lotteryNo);
 	}
 }
