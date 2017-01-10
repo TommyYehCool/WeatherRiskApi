f47033bcf99513807bbbbc86a48f8544465d3b2e
TommyYehCool <tommy.yeh1112@gmail.com>
2017-01-10 10:43:29 +0800

    新增 swagger 註解

--- src/main/java/com/weatherrisk/api/model/Consume.java
+++ src/main/java/com/weatherrisk/api/model/Consume.java
@@ -5,18 +5,37 @@ import java.util.Date;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import io.swagger.annotations.ApiModelProperty;
+
 @Document(collection = "consumes")
 public class Consume {
 	
 	@Id
+	@ApiModelProperty(notes = "發票號碼")
 	private String lotteryNo;
+	
+	@ApiModelProperty(notes = "使用者")
 	private String user;
+	
+	@ApiModelProperty(notes = "消費日期")
 	private Date consumeDate;
+	
+	@ApiModelProperty(notes = "類別")
 	private Integer type;
+	
+	@ApiModelProperty(notes = "商品名稱")
 	private String prodName;
+	
+	@ApiModelProperty(notes = "消費金額")
 	private Long amount;
+	
+	@ApiModelProperty(notes = "中獎金額")
 	private Long prize;
+	
+	@ApiModelProperty(notes = "是否中獎")
 	private Boolean got;
+	
+	@ApiModelProperty(notes = "是否已發信")
 	private Boolean alreadySent;
 	
 	public Consume() {
