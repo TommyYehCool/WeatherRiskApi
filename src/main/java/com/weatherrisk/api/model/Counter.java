92bae0129de6742c3c4661dd5c00b5e6731f5364
TommyYehCool <tommy.yeh1112@gmail.com>
2017-01-11 17:12:35 +0800

    新增取得下一筆序號的方法

---
+++ src/main/java/com/weatherrisk/api/model/Counter.java
@@ -0,0 +1,37 @@
+package com.weatherrisk.api.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "counters")
+public class Counter {
+	@Id
+	private String id;
+
+	private int seq;
+	
+	public Counter() {
+	}
+
+	public Counter(String id, int seq) {
+		this.id = id;
+		this.seq = seq;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public int getSeq() {
+		return seq;
+	}
+
+	public void setSeq(int seq) {
+		this.seq = seq;
+	}
+	
+}
