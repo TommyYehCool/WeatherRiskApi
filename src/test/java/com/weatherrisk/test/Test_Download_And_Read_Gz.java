de93ff03db291055b20d7a13251aff416cc7ca1f
TommyYehCool <tommy.yeh1112@gmail.com>
2017-03-09 10:59:04 +0800

    Modify. 多下載停車場資訊

--- src/test/java/com/weatherrisk/test/Test_Download_And_Read_Gz.java
+++ src/test/java/com/weatherrisk/test/Test_Download_And_Read_Gz.java
@@ -7,6 +7,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.zip.GZIPInputStream;
 
 import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
@@ -15,48 +16,62 @@ import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream;
 import org.apache.commons.compress.utils.IOUtils;
 import org.apache.commons.io.FileUtils;
 import org.junit.Test;
+import org.springframework.util.StringUtils;
 
 public class Test_Download_And_Read_Gz {
 	
-	private final String GET_FILE_URL = "https://tcgbusfs.blob.core.windows.net/blobtcmsv/TCMSV_allavailable.gz";
+	private final String PARKING_LOT_INFO_URL = "https://tcgbusfs.blob.core.windows.net/blobtcmsv/TCMSV_alldesc.gz";
 	
-	private final String FILE_LOCATION = "D://ParkingLotInfos.gz";
+	private final String PARKING_LOT_INFO_FILE_LOCATION = "D://ParkingLotInfo.gz";
+	
+	private final String PARKING_LOT_AVAILABLE_URL = "https://tcgbusfs.blob.core.windows.net/blobtcmsv/TCMSV_allavailable.gz";
+	
+	private final String PARKING_LOT_AVAILABLE_FILE_LOCATION = "D://ParkingLotAvailable.gz";
+	
+	@Test
+	public void testDownloadAndReadGz() {
+		try {
+			downloadFile(PARKING_LOT_INFO_URL, PARKING_LOT_INFO_FILE_LOCATION);
+			
+			readGzByJava(PARKING_LOT_INFO_FILE_LOCATION);
+			
+			downloadFile(PARKING_LOT_AVAILABLE_URL, PARKING_LOT_AVAILABLE_FILE_LOCATION);
+			
+			readGzByJava(PARKING_LOT_AVAILABLE_FILE_LOCATION);
+		} catch (IOException e) {
+			System.err.println("IOException rasied while trying to download file");
+			e.printStackTrace();
+		}
+	}
 	
 	/**
 	 * <pre>
 	 * 參考: <a href="http://stackoverflow.com/questions/921262/how-to-download-and-save-a-file-from-internet-using-java">Download and save a file from internet</a>
 	 * </pre>
 	 */
-	@Test
-	public void testDownloadAndReadGz() {
-		File outputFile = new File(FILE_LOCATION);
-		try {
-			if (outputFile.exists()) {
-				outputFile.delete();
-			}
-			outputFile.createNewFile();
+	private void downloadFile(String url, String fileLocation) throws IOException {
+		File fileParkingLotAvailable = new File(fileLocation);
+		if (fileParkingLotAvailable.exists()) {
+			fileParkingLotAvailable.delete();
+		}
+		fileParkingLotAvailable.createNewFile();
 
-			FileUtils.copyURLToFile(new URL(GET_FILE_URL), new File(FILE_LOCATION));
-			
-//			readGzByApache();
-			
-			readGzByJava();
-		}
-		catch (IOException e) {
-			e.printStackTrace();
-		}
+		FileUtils.copyURLToFile(new URL(url), new File(fileLocation));
 	}
 
 	/**
 	 * <pre>
 	 * 參考: <a href="http://stackoverflow.com/questions/25749550/read-tar-gz-in-java-with-commons-compression">Read tar/gz in java</a>
+	 * 
+	 * PS: 讀取會有問題
 	 * </pre>
 	 */
-	private void readGzByApache() {
+	@SuppressWarnings("unused")
+	private void readGzByApache(String fileLocation) {
 		TarArchiveInputStream tarInput = null;
 		BufferedReader br = null;
 		try {
-			tarInput = new TarArchiveInputStream(new GzipCompressorInputStream(new FileInputStream(FILE_LOCATION)));
+			tarInput = new TarArchiveInputStream(new GzipCompressorInputStream(new FileInputStream(fileLocation)));
 			TarArchiveEntry currentEntry = tarInput.getNextTarEntry();
 			while (currentEntry != null) {
 			    br = new BufferedReader(new InputStreamReader(tarInput)); // Read directly from tarInput
@@ -82,14 +97,20 @@ public class Test_Download_And_Read_Gz {
 	 * 參考: <a href="http://stackoverflow.com/questions/4991217/unzip-huge-gz-file-in-java-and-performance">Unzip gz file in java</a>
 	 * </pre>
 	 */
-	private void readGzByJava() {
+	private void readGzByJava(String fileLocation) {
 		BufferedReader br = null;
 		try {
-			br = new BufferedReader(new InputStreamReader(new GZIPInputStream(new FileInputStream(FILE_LOCATION))));
+			br = new BufferedReader(new InputStreamReader(new GZIPInputStream(new FileInputStream(fileLocation)), "MS950"));
+			
+			StringBuilder buffer = new StringBuilder();
+			
 			String line;
 			while ((line = br.readLine()) != null) {
-				System.out.println(line);
+				if (!StringUtils.isEmpty(line)) {
+					buffer.append(line);
+				}
 			}
+			System.out.println(buffer.toString());
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
