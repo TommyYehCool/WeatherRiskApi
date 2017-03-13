960860e85a6377bc0924d9261eee5b7789969d97
TommyYehCool <tommy.yeh1112@gmail.com>
2017-03-13 16:08:21 +0800

    調整 jaxb 寫法看在 heroku 上可不可以 work

--- src/main/java/com/weatherrisk/api/vo/xml/cwb/CwbOpenData.java
+++ src/main/java/com/weatherrisk/api/vo/xml/cwb/CwbOpenData.java
@@ -1,6 +1,9 @@
 package com.weatherrisk.api.vo.xml.cwb;
 
-import javax.xml.bind.annotation.*;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 
 import lombok.Data;
 
@@ -8,21 +11,30 @@ import lombok.Data;
 @XmlRootElement(name = "cwbopendata", namespace = "urn:cwb:gov:tw:cwbcommon:0.1")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class CwbOpenData {
+	@XmlElement
 	private String identifier;
 	
+	@XmlElement
 	private String sender;
 	
+	@XmlElement
 	private String sent;
 	
+	@XmlElement
 	private String status;
 	
+	@XmlElement
 	private String msgType;
 	
+	@XmlElement
 	private String scope;
 	
+	@XmlElement
 	private String dataid;
 	
+	@XmlElement
 	private String source;
 	
+	@XmlElement
 	private Dataset dataset;
 }
