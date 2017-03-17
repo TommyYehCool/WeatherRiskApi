78fc8d2a730487bf6ad75a0434e05571a47f3d7d
TommyYehCool <tommy.yeh1112@gmail.com>
2017-03-17 16:10:49 +0800

    1. 新增呼叫 google search 範例, 2. 新增 google custom search api 但還不會用

---
+++ src/test/java/com/weatherrisk/test/Test_GoogleCustomSearch.java
@@ -0,0 +1,16 @@
+package com.weatherrisk.test;
+
+/**
+ * https://developers.google.com/api-client-library/java/apis/customsearch/v1
+ * 
+ * @author tommy.feng
+ *
+ */
+public class Test_GoogleCustomSearch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
