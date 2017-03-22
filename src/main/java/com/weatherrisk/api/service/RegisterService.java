e2f031649e93c9ed73e629239514cfb0a405689f
TommyYehCool <tommy.yeh1112@gmail.com>
2017-03-22 13:49:29 +0800

    調整註冊機制

--- src/main/java/com/weatherrisk/api/service/RegisterService.java
+++ src/main/java/com/weatherrisk/api/service/RegisterService.java
@@ -1,22 +1,43 @@
 package com.weatherrisk.api.service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Service;
 
+import com.weatherrisk.api.cnst.CurrencyCnst;
 import com.weatherrisk.api.vo.PriceReached;
 
 @Service
 public class RegisterService {
 
-	private Map<String, PriceReached> cryptoRegisterMap = new HashMap<>();
+	private Map<String, List<PriceReached>> cryptoRegisterMap = new HashMap<>();
 	
 	public void register(String userId, PriceReached priceReached) {
-		this.cryptoRegisterMap.put(userId, priceReached);
+		List<PriceReached> pricesReached;
+		if (!this.cryptoRegisterMap.containsKey(userId)) {
+			pricesReached = new ArrayList<>();
+			pricesReached.add(priceReached);
+			this.cryptoRegisterMap.put(userId, pricesReached);
+		}
+		else {
+			pricesReached = this.cryptoRegisterMap.get(userId);
+			pricesReached.add(priceReached);
+		}
 	}
 	
 	public void unregister(String userId) {
 		this.cryptoRegisterMap.remove(userId);
+	}
+	
+	public boolean hasRegistered(String userId, CurrencyCnst currency) {
+		if (this.cryptoRegisterMap.containsKey(userId)) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 }
