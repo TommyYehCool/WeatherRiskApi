0646fc0d7f0e9e64eb1673ce1b6f26b7bcc57138
TommyYehCool <tommy.yeh1112@gmail.com>
2017-05-31 17:40:54 +0800

    調整格式錯誤說明

--- src/main/java/com/weatherrisk/api/line/LineMsgHandler.java
+++ src/main/java/com/weatherrisk/api/line/LineMsgHandler.java
@@ -524,7 +524,7 @@ public class LineMsgHandler {
     			}
     		}
     		else {
-    			queryResult = "格式錯誤, Ex: 2017/5/8 買貨幣 STR 0.00004900 20000";
+    			queryResult = "格式錯誤\n格式:日期 + 買/賣貨幣 + 幣別 + 價格 + 數量 + 手續費%數\nEx: 2017/5/8 買貨幣 STR 0.00004900 20000 0.15";
     		}
     	}
     	// 查詢貨幣庫存
